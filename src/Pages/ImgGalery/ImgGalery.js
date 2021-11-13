91843d0d46d15235da4b506d41ab9c682c90c74e
Md- Hasmat <mdhasmat690>
2021-11-13 13:16:13 +0600

    carousel text add

--- src/Pages/ImgGalery/ImgGalery.js
+++ src/Pages/ImgGalery/ImgGalery.js
@@ -13,7 +13,7 @@ const ImgGalery = () => {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3>Children's Large Foldable Bathtub</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -26,7 +26,7 @@ const ImgGalery = () => {
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3>Children's Large Foldable Bathtub</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -39,7 +39,7 @@ const ImgGalery = () => {
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>Children's Large Foldable Bathtub</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
